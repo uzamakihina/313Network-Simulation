54d3ba9ff23b370c8b90dca010489f428a174aa5
joe <jlx@ualberta.ca>
2019-04-01 12:51:04 -0600

    seqnum

--- lab3F.c
+++ lab3F.c
@@ -36,20 +36,8 @@ static int neighborsRemaining;
 static int EXPECT[33];
 static int SEQSEND[33];
 
-//static CnetTime lastTime;
-
-
-
-// static FRAME_D global_frame;
-// static size_t global_size;
-// static int global_link;
-// static int sendLink;
-
-
-
 static EVENT_HANDLER(display){
-  // for neighboring nodes - needs address, node name,
-  // for current node - simulation time, count_too_busy, informtion of packet being timed out.
+
   CNET_clear();
 
   for (int i =1; i <= nodeinfo.nlinks; i ++){
@@ -64,6 +52,7 @@ static EVENT_HANDLER(application_ready)
 
     CHECK(CNET_disable_application(ALLNODES));
     FRAME_D f;
+
 
     if (neighborsRemaining > 0){
       size_t length = MAX_MESSAGE_SIZE;
@@ -83,35 +72,29 @@ static EVENT_HANDLER(application_ready)
      else{
        size_t length = MAX_MESSAGE_SIZE;
        CnetAddr dest;
+
+       // CHECK IF the destination is for a server its connected to
+       //if not  exit out
        int linker = 0;
        CHECK(CNET_read_application(&dest, f.msg, &length));
-       //printf("Entering if statement nlinks = %d\n", nodeinfo.nlinks);
        for (int i = 1; i <= nodeinfo.nlinks ; i++){
          if (EveryNeighbor[i].address == dest){
-           //printf("FOUND LINK link = %d, dest = %d\n", i, dest);
            linker = i;
            break;
          }
        }
-
+       if (linker == 0) return ;
 
        f.kind = DL_DATA;
        f.checksum = 0;
        f.seq = SEQSEND[linker];
        f.nodeaddress = (int) nodeinfo.address;
-
+       strcpy(f.msg, nodeinfo.nodename);
        length = sizeof(FRAME_D);
        f.checksum  = CNET_ccitt((unsigned char *)&f, (int)length);
        CHECK(CNET_write_physical(linker,&f,&length));
-
-
-
      }
-
-
 }
-
-
 
 
  static EVENT_HANDLER(physical_ready){
@@ -120,21 +103,35 @@ static EVENT_HANDLER(application_ready)
     size_t length;
     int link;
     CNET_read_physical(&link, &f, &length);
+    if (nodeinfo.nodetype == NT_ROUTER){
+      int sender;
+      if (link == 1) sender = 2;
+      else {sender = 1;}
+
+      length = sizeof(FRAME_D);
+      CHECK(CNET_write_physical(sender,&f,&length));
+      return;
+    }
+
+
     int compare  = f.checksum;
     f.checksum = 0;
-    if (compare != CNET_ccitt((unsigned char *)&f, (int)length)) return;
+    if (compare != CNET_ccitt((unsigned char *)&f, (int)length)){
+      printf("Corrupt packet ignored\n");
+       return;}
 
     if (f.seq == EXPECT[link] && (f.kind == DISCOVER || f.kind == DL_DATA)){
       EXPECT[link] = 1 - EXPECT[link];
     }else if (f.seq == SEQSEND[link] && (f.kind == DISCOVER_ACK || f.kind == DL_ACK)){
       SEQSEND[link] = 1 - SEQSEND[link];
-    }else{printf("WRONG PACKET SEQ IGNORE\n");    }
+    }else{printf("WRONG PACKET SEQ IGNORE\n");}
 
     InfoTracker temp;
 
     switch(f.kind){
+
       case DISCOVER :
-        printf("seq = %d in discover\n", f.seq);
+        printf("Received discover request from link %d\n", link);
         f.nodeaddress = nodeinfo.address;
         strcpy(f.msg, nodeinfo.nodename);
         f.kind = DISCOVER_ACK;
@@ -144,27 +141,26 @@ static EVENT_HANDLER(application_ready)
         break;
 
       case DISCOVER_ACK:
-
         strcpy(temp.name, f.msg);
         temp.address = f.nodeaddress;
         EveryNeighbor[link] = temp;
-        printf("GOT AN ACK FROM NODEINFO REQUEST neightbor %d has name %s\n", EveryNeighbor[link].address, EveryNeighbor[link].name);
+        printf("  GOT AN ACK_DISCOVER FROM LINK %d with name %s\n", link, EveryNeighbor[link].name);
         neighborsRemaining -= 1;
         CNET_enable_application(ALLNODES);
         break;
 
       case DL_DATA:
-        printf(" Got data request \n");
+        printf("Got DL_DATA request from link %d with name %s\n", link, f.msg);
+        memset(f.msg,0,sizeof(f.msg));
+        strcpy(f.msg, nodeinfo.nodename);
         f.kind = DL_ACK;
         length = sizeof(FRAME_D);
         f.checksum = CNET_ccitt((unsigned char *)&f, (int)length);
         CHECK(CNET_write_physical(link,&f,&length));
-
-
         break;
-      case DL_ACK:
 
-        printf("GOT AN ACK FROM DATA REQUEST\n");
+      case DL_ACK:
+        printf("  GOT AN DL_ACK FROM LINK %d named %s\n", link, f.msg);
         CNET_enable_application(ALLNODES);
         break;
     }
@@ -182,19 +178,21 @@ EVENT_HANDLER(reboot_node)
       exit(1);
     }
 
-    CHECK(CNET_set_handler(EV_APPLICATIONREADY, application_ready, 0));
+
     CHECK(CNET_set_handler( EV_PHYSICALREADY,    physical_ready, 0));
+
+    if (nodeinfo.nodetype != NT_ROUTER){
     CHECK(CNET_set_handler( EV_TIMER1,           timeouts, 0));
-
-
     neighborsRemaining = nodeinfo.nlinks;
-  //  memset(EveryNeighbor,0,sizeof(EveryNeighbor));
+    CHECK(CNET_set_handler(EV_APPLICATIONREADY, application_ready, 0));
     CHECK(CNET_set_handler(EV_DEBUG0, display, 0));
     CHECK(CNET_set_debug_string(EV_DEBUG0, "TIMERS info"));
     CHECK(CNET_enable_application(ALLNODES));
     for (int i = 1; i <= 32; i++){
       SEQSEND[i] = 0;
       EXPECT[i] = 0;
+    }
+
     }
 
 
